b9d4d41d834e4959b69b1b4c977a6e283f71d267
Dhahbi Mohamed Amine <hamadhahbi2020@gmail.com>
2024-03-10 19:28:24 +0100

    first commit

---
+++ Frontend/src/app/etudiant/etudiant.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EtudiantRoutingModule } from './etudiant-routing.module';
+import { GestFichiersComponent } from './gest-fichiers/gest-fichiers.component';
+import { SharedModule } from './shared/shared.module';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderEtudiantComponent } from './header-etudiant/header-etudiant.component';
+
+
+@NgModule({
+  declarations: [GestFichiersComponent, DashbordComponent, MenuComponent, HeaderEtudiantComponent, ],
+  imports: [
+    CommonModule,
+    EtudiantRoutingModule,
+    SharedModule
+  ]
+})
+export class EtudiantModule { }
---
+++ Frontend/src/app/etudiant/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Matiere } from 'src/app/interfaces/matiere';
+import { EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  public lesmatieres: Array<Matiere> = [];
+  @Output() matiereSelected: EventEmitter<Matiere> = new EventEmitter();
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<{ matiere: Array<Matiere> }>('assets/db.json').subscribe(data => {
+      this.lesmatieres = data.matiere;
+    });
+  }
+
+  onMatiereSelected(matiere: Matiere): void {
+    this.matiereSelected.emit(matiere);
+  }
+}
